bb636c464ba35f82087975b40f8f3fe9b9c62940
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-02-01 20:03:39 -0300

    added middleware token and yup validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -9,6 +23,48 @@ class UserController {
     }
 
     const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string() // password com when(com dois parametros) - validação condicional - quando o oldpassword for preenchido password tem que ser obrigatorio
+        .min(6)
+        .when(
+          'oldPassword',
+          (oldPassword, field) => (oldPassword ? field.required() : field) // se a minha variavel oldPassword estiver preenchida eu quero que nosso field seja required, se nao retorna como estava antes, sem ser obrigatorio
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({
       id,
